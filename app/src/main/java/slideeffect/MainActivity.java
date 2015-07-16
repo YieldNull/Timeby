16b9259e26e4a680a62b537c8870864afd5fd9e8
fangdongliang <1780308918@qq.com>
2015-07-16 21:17:32 +0800

    好友列表

---
+++ app/src/main/java/slideeffect/MainActivity.java
@@ -0,0 +1,345 @@
+package slideeffect;
+
+import android.content.Context;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.nectar.timeby.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    List<String> parent = null;
+    Map<String, List<String>> map = null;
+    ExpandableListView mainlistview = null;
+    View view = null;
+
+
+    private ViewPager mPager;//页卡内容
+    private List<View> listViews; // Tab页面列表
+    private ImageView cursor;// 动画图片
+    private TextView t1, t2, t3;// 页卡头标
+    private int offset = 0;// 动画图片偏移量
+    private int currIndex = 0;// 当前页卡编号
+    private int bmpW;// 动画图片宽度
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainF);
+
+        InitImageView();
+        InitViewPager();
+        InitTextView();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * 初始化ViewPager
+     */
+    private void InitViewPager() {
+        mPager = (ViewPager) findViewById(R.id.vPager);
+        listViews = new ArrayList<View>();
+        LayoutInflater mInflater = getLayoutInflater();
+
+        view = mInflater.inflate(R.layout.lay1, null);
+        listViews.add(view);
+        mainlistview = (ExpandableListView) view.findViewById(R.id.main_expandablelistview);
+        mainlistview.setAdapter(new MyAdapter());
+
+
+
+        listViews.add(mInflater.inflate(R.layout.lay2, null));
+        listViews.add(mInflater.inflate(R.layout.lay3, null));
+
+
+
+        mPager.setAdapter(new MyPagerAdapter(listViews));
+
+        mPager.setCurrentItem(0);
+        mPager.setOnPageChangeListener(new MyOnPageChangeListener());
+
+    }
+
+    /**
+     * 初始化头标
+     */
+    private void InitTextView() {
+        t1 = (TextView) findViewById(R.id.text1);
+        t2 = (TextView) findViewById(R.id.text2);
+        t3 = (TextView) findViewById(R.id.text3);
+
+        t1.setOnClickListener(new MyOnClickListener(0));
+        t2.setOnClickListener(new MyOnClickListener(1));
+        t3.setOnClickListener(new MyOnClickListener(2));
+    }
+
+    /**
+     * 头标点击监听
+     */
+    public class MyOnClickListener implements View.OnClickListener {
+        private int index = 0;
+
+        public MyOnClickListener(int i) {
+            index = i;
+        }
+
+        @Override
+        public void onClick(View v) {
+            mPager.setCurrentItem(index);
+        }
+    }
+    /**
+     * ViewPager适配器
+     */
+
+    /**
+     * 初始化动画
+     */
+    private void InitImageView() {
+        cursor = (ImageView) findViewById(R.id.cursor);
+        bmpW = BitmapFactory.decodeResource(getResources(), R.drawable.a)
+                .getWidth();// 获取图片宽度
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int screenW = dm.widthPixels;// 获取分辨率宽度
+        offset = (screenW / 3 - bmpW) / 2;// 计算偏移量
+        Matrix matrix;
+        matrix = new Matrix();
+        matrix.postTranslate(offset, 0);
+        cursor.setImageMatrix(matrix);// 设置动画初始位置
+    }
+
+    public class MyPagerAdapter extends PagerAdapter {
+        public List<View> mListViews;
+
+        public MyPagerAdapter(List<View> mListViews) {
+            this.mListViews = mListViews;
+        }
+
+        @Override
+        public void destroyItem(View arg0, int arg1, Object arg2) {
+            ((ViewPager) arg0).removeView(mListViews.get(arg1));
+        }
+
+        @Override
+        public void finishUpdate(View arg0) {
+        }
+
+        @Override
+        public int getCount() {
+            return mListViews.size();
+        }
+
+        @Override
+        public Object instantiateItem(View arg0, int arg1) {
+            ((ViewPager) arg0).addView(mListViews.get(arg1), 0);
+            return mListViews.get(arg1);
+        }
+
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0 == (arg1);
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+        }
+
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+
+        @Override
+        public void startUpdate(View arg0) {
+        }
+    }
+    /**
+     * 页卡切换监听
+     */
+    public class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
+
+        int one = offset * 2 + bmpW;// 页卡1 -> 页卡2 偏移量
+        int two = one * 2;// 页卡1 -> 页卡3 偏移量
+
+        @Override
+        public void onPageSelected(int arg0) {
+            Animation animation = null;
+            switch (arg0) {
+                case 0:
+                    if (currIndex == 1) {
+                        animation = new TranslateAnimation(one, 0, 0, 0);
+                    } else if (currIndex == 2) {
+                        animation = new TranslateAnimation(two, 0, 0, 0);
+                    }
+                    break;
+                case 1:
+                    if (currIndex == 0) {
+                        animation = new TranslateAnimation(offset, one, 0, 0);
+                    } else if (currIndex == 2) {
+                        animation = new TranslateAnimation(two, one, 0, 0);
+                    }
+                    break;
+                case 2:
+                    if (currIndex == 0) {
+                        animation = new TranslateAnimation(offset, two, 0, 0);
+                    } else if (currIndex == 1) {
+                        animation = new TranslateAnimation(one, two, 0, 0);
+                    }
+                    break;
+            }
+            currIndex = arg0;
+            animation.setFillAfter(true);// True:图片停在动画结束位置
+            animation.setDuration(300);
+            cursor.startAnimation(animation);
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+        }
+    }
+//填充子LISTVIEW
+    public void initData() {
+        parent = new ArrayList<String>();
+        parent.add("parent1");
+        map = new HashMap<String, List<String>>();
+
+        List<String> list1 = new ArrayList<String>();
+        list1.add("child1-1");
+        list1.add("child1-2");
+        list1.add("child1-3");
+        map.put("parent1", list1);
+    }
+    class MyAdapter extends BaseExpandableListAdapter {
+
+        //得到子item需要关联的数据
+        @Override
+        public Object getChild(int groupPosition, int childPosition) {
+            String key = parent.get(groupPosition);
+            return (map.get(key).get(childPosition));
+        }
+
+        //得到子item的ID
+        @Override
+        public long getChildId(int groupPosition, int childPosition) {
+            return childPosition;
+        }
+
+        //设置子item的组件
+        @Override
+        public View getChildView(int groupPosition, int childPosition,
+                                 boolean isLastChild, View convertView, ViewGroup parent) {
+            String key =MainActivity.this.parent.get(groupPosition);
+            String info = map.get(key).get(childPosition);
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) MainActivity.this
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.child_item, null);
+            }
+            TextView tv = (TextView) convertView
+                    .findViewById(R.id.second_textview);
+            tv.setText(info);
+            return tv;
+        }
+        public int getChildrenCount(int groupPosition) {
+            String key = parent.get(groupPosition);
+            int size=map.get(key).size();
+            return size;
+        }
+        //获取当前父item的数据
+        @Override
+        public Object getGroup(int groupPosition) {
+            return parent.get(groupPosition);
+        }
+
+        @Override
+        public int getGroupCount() {
+            return parent.size();
+        }
+
+        @Override
+        public long getGroupId(int groupPosition) {
+            return groupPosition;
+        }
+        //设置父item组件
+        @Override
+        public View getGroupView(int groupPosition, boolean isExpanded,
+                                 View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater inflater = (LayoutInflater) MainActivity.this
+                        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(R.layout.parent_item, null);
+            }
+            TextView tv = (TextView) convertView
+                    .findViewById(R.id.parent_textview);
+            tv.setText(MainActivity.this.parent.get(groupPosition));
+            return tv;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public boolean isChildSelectable(int groupPosition, int childPosition) {
+            return true;
+        }
+
+    }
+
+}
+
+
+
+
