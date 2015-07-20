b79144dfce8040f37fb2db9527ffa3612e0f8f87
上白泽丶稻叶 <shangbaize-daoye@qq.com>
2015-07-20 11:54:59 +0800

    add task type select dialog

--- app/src/main/java/com/nectar/timeby/gui/fragment/MainFragment.java
+++ app/src/main/java/com/nectar/timeby/gui/fragment/MainFragment.java
@@ -7,14 +7,21 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -24,6 +31,7 @@ import com.nectar.timeby.R;
 import com.nectar.timeby.gui.MainSingleActivity;
 import com.nectar.timeby.gui.util.OnDrawerStatusChangedListener;
 import com.nectar.timeby.gui.util.OnDrawerToggleClickListener;
+
 
 import java.util.Calendar;
 
@@ -62,6 +70,7 @@ public class MainFragment extends Fragment
     public MainFragment() {
 
     }
+
 
     @Override
     public void onDrawerOpening() {
@@ -124,17 +133,20 @@ public class MainFragment extends Fragment
         mSubmitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Bundle bundle = new Bundle();
-                bundle.putInt("CountDownTime", sumMin);
-                bundle.putInt("startHour", startHour);
-                bundle.putInt("startMin", startMin);
-                bundle.putInt("endHour", endHour);
-                bundle.putInt("endMin", endMin);
-                bundle.putString("startAPM", mStartAPMText.getText().toString());
-                bundle.putString("endAPM", mEndAPMText.getText().toString());
-                Intent intToSingle = new Intent(getActivity(), MainSingleActivity.class);
-                intToSingle.putExtras(bundle);
-                startActivity(intToSingle);
+                TaskTypeSelectDialog taskTypeSelectDialog=new TaskTypeSelectDialog(getActivity());
+                taskTypeSelectDialog.showDialog();
+
+//                Bundle bundle = new Bundle();
+//                bundle.putInt("CountDownTime", sumMin);
+//                bundle.putInt("startHour", startHour);
+//                bundle.putInt("startMin", startMin);
+//                bundle.putInt("endHour", endHour);
+//                bundle.putInt("endMin", endMin);
+//                bundle.putString("startAPM", mStartAPMText.getText().toString());
+//                bundle.putString("endAPM", mEndAPMText.getText().toString());
+//                Intent intToSingle = new Intent(getActivity(), MainSingleActivity.class);
+//                intToSingle.putExtras(bundle);
+//                startActivity(intToSingle);
             }
         });
 
@@ -265,6 +277,263 @@ public class MainFragment extends Fragment
 
     //时钟 by 上白泽稻叶
     //以下是时钟的控件与属性
+
+    class TaskTypeSelectDialog {
+        public static final int SOLO=0;
+        public static final int COOP=1;
+        public static final int PK=2;
+        private Activity activity;
+
+        private int windowWidth;
+        private int windowHeight;
+        private int dialogHeight;
+        private RelativeLayout windowLayout;
+        private RelativeLayout dialogLayout;
+        private TextView solo;
+        private TextView coop;
+        private TextView pk;
+
+        private void startSolo(){
+            //
+            //在此填上点击单人按钮后的动作
+            //
+        }
+        private void startCoop(){
+            //
+            //在此填上点击合作按钮后的动作
+            //
+        }
+        private void startPK(){
+            //
+            //在此填上点击PK按钮后的动作
+            //
+        }
+
+        private void removeDialog(){
+            mSubmitButton.setEnabled(true);
+            Animation dialogDisappear = new TranslateAnimation(0, 0, 0, dialogHeight);
+            dialogDisappear.setDuration(500);
+            dialogDisappear.setFillEnabled(true);
+            dialogDisappear.setFillAfter(true);
+            dialogDisappear.setInterpolator(new LinearInterpolator());
+            dialogDisappear.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {
+
+                }
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    ((ViewGroup)windowLayout.getParent()).removeView(windowLayout);
+                }
+
+                @Override
+                public void onAnimationRepeat(Animation animation) {
+
+                }
+            });
+            dialogLayout.startAnimation(dialogDisappear);
+        }
+
+        public TaskTypeSelectDialog(Activity activity) {
+            this.activity = activity;
+
+            windowWidth = activity.getWindowManager().getDefaultDisplay().getWidth();
+            dialogHeight = (int) (0.18 * activity.getWindowManager().getDefaultDisplay().getHeight());
+        }
+
+        public void showDialog() {
+            mSubmitButton.setEnabled(false);
+
+            windowLayout = new RelativeLayout(activity);
+            RelativeLayout.LayoutParams windowLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+            dialogLayout = new RelativeLayout(activity);
+            dialogLayout.setBackgroundColor(Color.rgb(173, 204, 173));
+            RelativeLayout.LayoutParams dialogLayoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, dialogHeight);
+            dialogLayoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+            TextView selectTaskType = new TextView(activity);
+            selectTaskType.setText(R.string.select_task_type);
+            selectTaskType.setTextColor(Color.rgb(255, 255, 255));
+            selectTaskType.setGravity(Gravity.BOTTOM);
+            selectTaskType.setTextSize(dialogHeight / 10);
+            RelativeLayout.LayoutParams selectTaskTypeParams = new RelativeLayout.LayoutParams((int) (windowWidth * 0.9), (int) (dialogHeight * 0.33));
+            selectTaskTypeParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
+            selectTaskTypeParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+            solo = new TextView(activity);
+            solo.setText(R.string.task_type_solo);
+            solo.setTextColor(Color.rgb(255, 255, 227));
+            solo.setTextSize(dialogHeight / 6);
+            solo.setGravity(Gravity.CENTER);
+            RelativeLayout.LayoutParams soloParams = new RelativeLayout.LayoutParams(windowWidth / 3, (int) (dialogHeight * 0.67));
+            soloParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+            soloParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+            coop = new TextView(activity);
+            coop.setText(R.string.task_type_coop);
+            coop.setTextSize(dialogHeight / 6);
+            coop.setTextColor(Color.rgb(255, 255, 227));
+            coop.setGravity(Gravity.CENTER);
+            RelativeLayout.LayoutParams coopParams = new RelativeLayout.LayoutParams(windowWidth / 3, (int) (dialogHeight * 0.67));
+            coopParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+            coopParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+            pk = new TextView(activity);
+            pk.setText(R.string.task_type_pk);
+            pk.setTextSize(dialogHeight / 6);
+            pk.setTextColor(Color.rgb(255, 255, 227));
+            pk.setGravity(Gravity.CENTER);
+            RelativeLayout.LayoutParams pkParams = new RelativeLayout.LayoutParams(windowWidth / 3, (int) (dialogHeight * 0.67));
+            pkParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+            pkParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+
+            dialogLayout.addView(selectTaskType, selectTaskTypeParams);
+            dialogLayout.addView(solo, soloParams);
+            dialogLayout.addView(coop, coopParams);
+            dialogLayout.addView(pk, pkParams);
+            windowLayout.addView(dialogLayout, dialogLayoutParams);
+            activity.addContentView(windowLayout, windowLayoutParams);
+
+            ViewTreeObserver vto = windowLayout.getViewTreeObserver();
+            vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                @Override
+                public void onGlobalLayout() {
+                    windowLayout.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    windowHeight = windowLayout.getHeight();
+                }
+            });
+
+            Animation popUp = new TranslateAnimation(0, 0, dialogHeight, 0);
+            popUp.setDuration(500);
+            popUp.setInterpolator(new DecelerateInterpolator());
+            popUp.setAnimationListener(new PopUpEndListener());
+            dialogLayout.startAnimation(popUp);
+        }
+
+        private class PopUpEndListener implements Animation.AnimationListener {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                solo.setOnTouchListener(new SelectTaskTypeListener(TaskTypeSelectDialog.SOLO));
+                coop.setOnTouchListener(new SelectTaskTypeListener(TaskTypeSelectDialog.COOP));
+                pk.setOnTouchListener(new SelectTaskTypeListener(TaskTypeSelectDialog.PK));
+                windowLayout.setOnTouchListener(new dialogCancelListener());
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        }
+
+        private class SelectTaskTypeListener implements View.OnTouchListener {
+            private int taskType;
+
+            SelectTaskTypeListener(int taskType){
+                this.taskType=taskType;
+            }
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int eventType=event.getAction();
+                if(taskType==TaskTypeSelectDialog.SOLO){
+                    if(eventType==MotionEvent.ACTION_DOWN){
+                        solo.setBackgroundColor(Color.rgb(227, 225, 223));
+                        solo.getPaint().setFakeBoldText(true);
+                        solo.setTextColor(Color.rgb(130, 180, 130));
+                        return true;
+                    }
+                    else if(eventType==MotionEvent.ACTION_MOVE){
+                        return true;
+                    }
+                    else if(eventType==MotionEvent.ACTION_UP){
+                        solo.setBackgroundColor(Color.argb(0, 255, 255, 255));
+                        solo.getPaint().setFakeBoldText(false);
+                        solo.setTextColor(Color.rgb(255, 255, 227));
+                        int pointX=(int)event.getX();
+                        int pointY=(int)event.getY();
+                        if(pointX>0&&pointX<windowWidth/3&&pointY>0&&pointY<(int)(dialogHeight*0.67)){
+                            startSolo();
+                            removeDialog();
+                        }
+                        return false;
+                    }
+                }
+                else if(taskType==TaskTypeSelectDialog.COOP){
+                    if(eventType==MotionEvent.ACTION_DOWN){
+                        coop.setBackgroundColor(Color.rgb(227, 225, 223));
+                        coop.getPaint().setFakeBoldText(true);
+                        coop.setTextColor(Color.rgb(130, 180, 130));
+                        return true;
+                    }
+                    else if(eventType==MotionEvent.ACTION_MOVE){
+                        return true;
+                    }
+                    else if(eventType==MotionEvent.ACTION_UP){
+                        coop.setBackgroundColor(Color.argb(0, 255, 255, 255));
+                        coop.getPaint().setFakeBoldText(false);
+                        coop.setTextColor(Color.rgb(255, 255, 227));
+                        int pointX=(int)event.getX();
+                        int pointY=(int)event.getY();
+                        if(pointX>0&&pointX<windowWidth/3&&pointY>0&&pointY<(int)(dialogHeight*0.67)){
+                            startCoop();
+                            removeDialog();
+                        }
+                        return false;
+                    }
+                }
+                else if(taskType==TaskTypeSelectDialog.PK){
+                    if(eventType==MotionEvent.ACTION_DOWN){
+                        pk.setBackgroundColor(Color.rgb(227, 225, 223));
+                        pk.getPaint().setFakeBoldText(true);
+                        pk.setTextColor(Color.rgb(130, 180, 130));
+                        return true;
+                    }
+                    else if(eventType==MotionEvent.ACTION_MOVE){
+                        return true;
+                    }
+                    else if(eventType==MotionEvent.ACTION_UP){
+                        pk.setBackgroundColor(Color.argb(0, 255, 255, 255));
+                        pk.getPaint().setFakeBoldText(false);
+                        pk.setTextColor(Color.rgb(255, 255, 227));
+                        int pointX=(int)event.getX();
+                        int pointY=(int)event.getY();
+                        if(pointX>0&&pointX<windowWidth/3&&pointY>0&&pointY<(int)(dialogHeight*0.67)){
+                            startPK();
+                            removeDialog();
+                        }
+                        return false;
+                    }
+                }
+                else{
+                    System.out.println("The task type in SelectTaskTypeListener wasn't set correctly!");
+                    return false;
+                }
+                return false;
+            }
+        }
+
+        private class dialogCancelListener implements View.OnTouchListener{
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int pointX=(int)event.getX();
+                int pointY=(int)event.getY();
+                if(pointX>0&&pointX<windowWidth&&pointY>0&&pointY<windowHeight-dialogHeight){
+                    removeDialog();
+                }
+                return false;
+            }
+        }
+    }
+
     private MinuteHandView minuteHandView = null;
     private HourHandView hourHandView = null;
     private double currMinuHandDegr = 0.0;
